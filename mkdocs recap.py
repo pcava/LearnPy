2f3be2f63a988f465e27cba2a76f8c48f4dc584b
pcavatore <pcavatore@gmail.com>
2024-08-05 01:35:25 +0200

    Add mkdocs

--- mkdocs recap.py
+++ mkdocs recap.py
@@ -3,5 +3,23 @@
 # conda install conda-forge::mkdocs
 # conda install conda-forge::mkdocs-material
 
-# Create the working folder
-mkdocs new "<your GitHub username>.github.io"
+# Create the working folder - run in terminal:
+# mkdocs new "pcava.github.io"
+# mkdocs new . # in case you have already created the pcava.github.io folder and are running this from within that folder
+
+# Adjust the mkdocs.yml 
+
+# To preview your website with live changes via local URL:
+# mkdocs serve
+
+# Create a pcava/pcava.github.io repository in GitHub as per https://pages.github.com/
+
+# When happy deploy:
+# mkdocs gh-deploy
+
+# and commit to pcava.github.io:
+# git add .
+# git commit -m "Create website"
+
+# and push to GitHub:
+# git push origin master
